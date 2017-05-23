c69f1e327ac14a0649af2b8851e0e00ed23f5aac
laohu <cswyjd@163.com>
2017-05-23 20:34:36 +0800

    init git

---
+++ app/src/main/java/framekit/demo/com/helloworld_jenkins/MainActivity.java
@@ -0,0 +1,16 @@
+package framekit.demo.com.helloworld_jenkins;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"hello world!!",Toast.LENGTH_LONG).show();
+    }
+}
